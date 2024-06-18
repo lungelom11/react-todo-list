19a1566627628f05cd0726560d9aff877d84820f
Lungelo <lungelomkhwemte11@gmail.com>
2024-06-18 19:23:37 +0200

    First commit, and Add item component

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./style.css";
+
+const App = () => {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: true },
+      ];
+    });
+  }
+
+  console.log(todos);
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
+          <button className="btn">Add</button>
+        </div>
+      </form>
+      <h1 className="header">List Items</h1>
+      <ul className="list">
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <label>
+              <input type="checkbox" checked={todo.completed} />
+              {todo.title}
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default App;
